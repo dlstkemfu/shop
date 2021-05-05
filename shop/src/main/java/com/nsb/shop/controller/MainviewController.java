ece62e0dd01d222315d382c747246a9f83457119
dlstkemfu <dlstkemfu@naver.com>
2021-05-05 21:44:16 +0900

    2021-05-05

--- shop/src/main/java/com/nsb/shop/controller/MainviewController.java
+++ shop/src/main/java/com/nsb/shop/controller/MainviewController.java
@@ -9,7 +9,20 @@ public class MainviewController {
 @RequestMapping("/")
 public String mainview(Model model) {
 model.addAttribute("title", "알뜰시장");
-
+model.addAttribute("id","아이디:");
+model.addAttribute("pw","패스워드:");
 return "mainview";
+}
+@RequestMapping("/login")
+public String login(Model model) {
+
+
+return "login";
+}
+@RequestMapping("/signup")
+public String signup(Model model) {
+
+
+return "signup";
 }
 }
--- shop/src/main/webapp/WEB-INF/views/mainview.jsp
+++ shop/src/main/webapp/WEB-INF/views/mainview.jsp
@@ -12,7 +12,11 @@ charset=UTF-8"
 <body>
 
 	<h1>${title}</h1>
-
-	</div>
+	<h3>${id } <input type="text" id="id" name="id" placeholder="아이디입력"></h3>
+	<h3>${pw}	<input type="text" id="pw" name="pw" placeholder="비밀번호입력"></h3>
+	
+	
+<a href="${path}/shop/login" >로그인하기</a> <a href="${path}/shop/signup" >회원가입하러가기</a>
+	
 </body>
 </html>
